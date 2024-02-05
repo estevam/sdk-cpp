244eeec498b6746ffad067be9ff2f38eeee6e8e3
Estevam Meneses <estevammeneses@gmail.com>
2024-02-05 12:36:04 -0500

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,7 +14,8 @@
 #include <boost/uuid/uuid.hpp>            // uuid class
 #include <boost/uuid/uuid_generators.hpp> // generators
 #include <boost/uuid/uuid_io.hpp> 
-
+#include <unordered_map>
+#include <served/served.hpp>
 using namespace std;
 
 
@@ -70,5 +71,26 @@ cout << uuid;
     cout << "Call singleton class will print the same address:  \n ";
 	std::cout << s << std::endl;
 	std::cout << r << std::endl;
+
+	unordered_map < int, string > hashmap;
+
+	// REST
+    served::multiplexer mux;
+    mux.handle("/api/v1/greeting")
+        .get([&](served::response &res, const served::request &req) {
+            std::string name = req.query["name"];
+            res.set_header("content-type", "application/json");
+            res << "{ \"content\": \"Hello, " << ((name.length() > 0) ? name : "world") << "!\" }\n";
+        });
+
+    std::cout << "Try this example with:" << std::endl;
+    std::cout << "  curl \"http://localhost:8123/api/v1/greeting?name=world\"" << std::endl;
+
+    served::net::server server("127.0.0.1", "8123", mux);
+    server.run(10);
+
+    return (EXIT_SUCCESS);
+
+
 	return 0;
 }
